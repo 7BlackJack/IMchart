d4d305f780c34cfc5ded7c68d81b8961c15cc8ec
云影 <efarxs@163.com>
2023-12-18 13:03:01 +0800

    自动识别https还是http协议，自动选择wss还是ws通信

---
+++ src/main/java/com/imchat/server/vo/UserVo.java
@@ -0,0 +1,45 @@
+package com.imchat.server.vo;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * @Description
+ * @Author Efar <efarxs@163.com>
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2023/11/26
+ */
+@Data
+public class UserVo {
+    /**
+     *
+     */
+    private Integer id;
+
+    /**
+     *
+     */
+    private String username;
+
+
+    /**
+     *
+     */
+    private Integer status;
+
+
+    /**
+     *
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+}
